a7dac54766245ba5daeb51fd62d8051be704362a
Grayson Howard <mhoward@mtu.edu>
2022-04-19 10:34:23 -0400

    Initial commit

---
+++ src/main/java/dev/gray/App.java
@@ -0,0 +1,17 @@
+package dev.gray;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create();
+
+        app.get("/add/{num1}/{num2}", context ->{
+            int num1 = Integer.parseInt(context.pathParam("num1"));
+            int num2 = Integer.parseInt(context.pathParam("num2"));
+            context.result("The sum is: " + (num1+num2));
+        });
+
+        app.start(7200);
+    }
+}
